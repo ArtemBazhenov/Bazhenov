d8d30b338d8bb289cd01103a0199250ab7b20a23
ArtemBazhenov <ar.bazhenov@gmail.com>
2022-01-11 21:41:55 +0300

    Create Unittest.py

---
+++ Unittest.py
@@ -0,0 +1,31 @@
+from class_DB import DB
+import unittest
+
+class TestTable(unittest.TestCase):
+    
+    def setUpClass(cls) -> None:
+        cls.db = DB()
+    
+    def tearDownClass(cls) -> None:
+        cls.db.dbClose()
+    
+    def setUp(self):
+        self.db.newCursor()
+    
+    def tearDown(self):
+        self.db.closeCursor()
+    
+    def test_get1(self):
+        res = [(1, 'towel-pants', 3), (2, 'towel-pants', 3), (3, 'surprisebox', 1), (3, 'towel-pants', 1)]
+        self.assertEqual(self.db.get1(), res)
+    
+    def test_get2(self):
+        res = [('Oleg', '+'), ('Alina', '+'), ('Artem', '+')]
+        self.assertEqual(self.db.get2(), res)
+        
+    def test_get3(self):
+        res = [('surprisebox', 'mystic'), ('towel-pants', 'clother')]
+        self.assertEqual(self.db.get3(), res)
+    
+if __name__ == '__main__':
+    unittest.main(failfast = False)
